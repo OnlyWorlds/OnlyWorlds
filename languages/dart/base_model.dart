2d40a31b320a8d26b0b3fe7ec38244860b8c0e33
Titus <titusoosting@gmail.com>
2025-03-16 20:15:56 +0100

    fix dart

--- languages/dart/base_model.dart
+++ languages/dart/base_model.dart
@@ -1,9 +1,8 @@
-
 abstract class BaseModel {
   String id;
   String name;
   String description;
   String supertype;
   String subtype;
-  String imageURL;
+  String imageurl;
 }
--- languages/dart/base_properties.dart
+++
@@ -1,8 +0,0 @@
-class Base_properties extends BaseModel {
-  final String Id;
-  final String Name;
-  final String Description;
-  final String Supertype;
-  final String Subtype;
-  final String Image_URL;
-}
